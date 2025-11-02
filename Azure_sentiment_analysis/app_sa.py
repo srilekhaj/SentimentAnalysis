cefca46f8cc52f9f99d6cf1e9d4080340edcc4d8
Srilekha J <56781872+srilekhaj@users.noreply.github.com>
2025-11-02 12:32:43 +0530

    Update app_sa.py

--- Azure_sentiment_analysis/app_sa.py
+++ Azure_sentiment_analysis/app_sa.py
@@ -12,8 +12,7 @@ AI_SERVICE_ENDPOINT = os.getenv("AI_SERVICE_ENDPOINT")
 credential = AzureKeyCredential(AI_SERVICE_KEY)
 ai_client = TextAnalyticsClient(endpoint=AI_SERVICE_ENDPOINT, credential=credential)
 
-#OPENAI_API_ENDPOINT = "https://aiservicesdemo3232.cognitiveservices.azure.com/openai/deployments/gpt-35-turbo/chat/completions?api-version=2024-08-01-preview"  # Replace with your actual OpenAI API key
-OPENAI_API_ENDPOINT = "https://aiservicesdemo3232.openai.azure.com/"
+OPENAI_API_ENDPOINT = os.getenv("OPENAI_API_ENDPOINT")
 # OpenAI API setup
 client = AzureOpenAI(
     azure_endpoint= OPENAI_API_ENDPOINT, 
@@ -64,31 +63,6 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# import streamlit as st
-# from azure.core.credentials import AzureKeyCredential
-# from azure.ai.textanalytics import TextAnalyticsClient
-
-# # Initialize Azure Text Analytics client
-# AI_SERVICE_ENDPOINT = "https://aiservicesdemo3232.cognitiveservices.azure.com/"
-# AI_SERVICE_KEY = "dad9815a11fb41adb8983c5a7f42a145"
-
-# ai_endpoint = AI_SERVICE_ENDPOINT
-# ai_key = AI_SERVICE_KEY
-# credential = AzureKeyCredential(ai_key)
-# ai_client = TextAnalyticsClient(endpoint=ai_endpoint, credential=credential)
-
-# # Streamlit app
-# st.title("Sentiment Analysis")
-
-# # Text input
-# user_input = st.text_area("Enter text for sentiment analysis:")
-
-# if st.button("Analyze Sentiment"):
-#     if user_input:
-#         # Analyze sentiment
-#         sentiment_analysis = ai_client.analyze_sentiment(documents=[user_input])[0]
-#         sentiment = sentiment_analysis.sentiment
-#         st.write(f"Sentiment: **{sentiment}**")
-#     else:
+    
 #         st.error("Please enter some text.")
+
